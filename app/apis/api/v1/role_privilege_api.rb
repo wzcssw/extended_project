dab6727e7fe066b3e3e484f278e4046bb5852df2
王志成 <wangzhicheng@tongxinyiliao.com>
2016-08-19 10:52:57 +0800

    增加privilege_list

--- app/apis/api/v1/role_privilege_api.rb
+++ app/apis/api/v1/role_privilege_api.rb
@@ -25,6 +25,31 @@ module API
         present :current_page, @roles.current_page
       end
 
+      desc "权限列表"
+      params do
+        optional :page, type: Integer
+        optional :limit, type: Integer
+        optional :q, type: String
+        optional :role_id, type: Integer
+      end
+      get :privilege_list do
+        page = params[:page] || 1
+        limit = params[:limit] || 20
+        q = params[:q] || ''
+        role_id = params[:role_id]
+
+        @privileges =  Privilege.page(page).per(limit) if (role_id.blank? && q.blank?)
+        @privileges =  Privilege.joins(:roles).where("role_id=?",role_id).page(page).per(limit) if role_id.present?
+        @privileges =  Privilege.where(name: q).page(page).per(limit) if q.present?
+
+        present :privilege, @privileges
+        present :total_count, @privileges.total_count
+        present :prev_page, @privileges.prev_page
+        present :next_page, @privileges.next_page
+        present :total_pages, @privileges.total_pages
+        present :current_page, @privileges.current_page
+      end
+
      end
   end
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -15,5 +15,7 @@ class Test
     puts "http://localhost:3000/api/v1/users/delete?app_token=#{app_token}&access_token=#{access_token}&timestemp=#{timestemp}&sign=#{sign}&id="
     puts "[GET]"
     puts "http://localhost:3000/api/v1/role_privileges/role_list?app_token=#{app_token}&access_token=#{access_token}&timestemp=#{timestemp}&sign=#{sign}"
+    puts "[GET]"
+    puts "http://localhost:3000/api/v1/role_privileges/privilege_list?app_token=#{app_token}&access_token=#{access_token}&timestemp=#{timestemp}&sign=#{sign}"
   end
 end
